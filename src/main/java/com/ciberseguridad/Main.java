9f9519bc0d47e0c3e70cc965450e8171200b2945
sergi <sergio_socha@yahoo.com>
2025-01-29 12:36:36 -0500

    agrega Rockyou, agrega clases cifrado

--- src/main/java/com/ciberseguridad/Main.java
+++ src/main/java/com/ciberseguridad/Main.java
@@ -2,6 +2,24 @@ package com.ciberseguridad;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+
+
+
+
+    }
+
+
+    public static void CifradoCesar(String palabra, int clave){
+        palabra = palabra.toLowerCase();
+
+
+    }
+
+    public static void CifradoVigenere(String palabra, String clave ){
+        palabra = palabra.toLowerCase();
+
+
     }
 }
