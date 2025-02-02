be4adb4e531314cb7a6c965e43f32b23ab9a6f09
sergi <sergio_socha@yahoo.com>
2025-02-02 12:53:39 -0500

    Agrega clases de cifrado

--- src/main/java/com/ciberseguridad/Main.java
+++ src/main/java/com/ciberseguridad/Main.java
@@ -1,6 +1,7 @@
 package com.ciberseguridad;
 
 import javax.swing.event.InternalFrameAdapter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -13,35 +14,76 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido al programa de cifrado");
-        System.out.println("Ingrese la palabra: ");
-        String palabra = sc.nextLine();
-        System.out.println("Ingrese la clave: ");
-        int clave= sc.nextInt();
+        System.out.println("Menu Elija que opcion desea" +
+                "1. Cifrado Cesar" +
+                "2. Descifrado Cesar" +
+                "3. Cifrado Vigenere" +
+                "4. Descrifrado Vigenere");
 
-        CifradoCesar(palabra,clave);
+        int menu = sc.nextInt();
+        switch (menu){
+            case 1:
+
+                System.out.println("Ingrese la palabra: ");
+                String palabra = sc.nextLine();
+
+                System.out.println("Ingrese la clave: ");
+                int clave= sc.nextInt();
+
+                CifradoCesar(palabra,clave);
+
+                String resultado = CifradoCesar(palabra, clave);
+                System.out.println("Palabra cifrada: " + resultado);
+
+                break;
+            case 2:
+
+                break;
+        }
+
+
+
+
     }
 
 
-    public static void CifradoCesar(String palabra, int clave){
+    public static String CifradoCesar(String palabra, int clave) {
         palabra = palabra.toLowerCase();
         char[] arrPalabra = palabra.toCharArray();
-        char[] decifrada = new char[0];
-        for(int i = 0; i < palabra.length(); i++){
-            for(int j = i + 1; j <= i; j++) {
-                if (arrPalabra[i] == alf[j]){
-                    decifrada[i] = alf[j + clave];
+        ArrayList<Character> cifrada = new ArrayList<>();
+
+        for (char letra : arrPalabra) {
+
+            if (letra >= 'a' && letra <= 'z') {
+                int posicionActual = letra - 'a';
+                int nuevaPosicion = (posicionActual + clave) % 26;
+                if (nuevaPosicion < 0) {
+                    nuevaPosicion += 26;
                 }
+                cifrada.add(alf[nuevaPosicion]);
+            } else {
+                cifrada.add(letra);
             }
-
-
         }
-        System.out.println( decifrada);
+
+
+        StringBuilder resultado = new StringBuilder(cifrada.size());
+        for (Character ch : cifrada) {
+            resultado.append(ch);
+        }
+
+        return resultado.toString();
+    }
+
+    public static void DescifradoCesar(String palabra, String clave){
+
+
 
     }
 
     public static void CifradoVigenere(String palabra, String clave ){
         palabra = palabra.toLowerCase();
 
-
     }
+
 }
