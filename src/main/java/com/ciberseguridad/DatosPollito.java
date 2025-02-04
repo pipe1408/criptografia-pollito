a8c80dc010d66090afd7da844ec8b69922ee3555
Felipe <pipeba1408@gmail.com>
2025-02-04 10:41:23 -0500

    Implementa navegación

--- src/main/java/com/ciberseguridad/DatosPollito.java
+++ src/main/java/com/ciberseguridad/DatosPollito.java
@@ -2,7 +2,6 @@ package com.ciberseguridad;
 
 public class DatosPollito {
 
-
     public static final String[] ALFABETO = {"a","b","c","d","e","f","g","h","i","j","k","l",
             "m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 
--- src/main/java/com/ciberseguridad/Main.java
+++ src/main/java/com/ciberseguridad/Main.java
@@ -1,47 +1,72 @@
 package com.ciberseguridad;
 
-import javax.swing.event.InternalFrameAdapter;
 import java.util.Scanner;
 
 public class Main {
-
-
 
     static char[] alf = {'a','b','c','d','e','f','g','h','i','j','k','l'
             ,'m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Bienvenido al programa de cifrado");
-        System.out.println("Ingrese la palabra: ");
-        String palabra = sc.nextLine();
-        System.out.println("Ingrese la clave: ");
-        int clave= sc.nextInt();
 
-        CifradoCesar(palabra,clave);
+        String menuText = """
+                Bienvenido al programa de criptografía Pollitoconpapas
+                
+                ======================
+                    Menú Principal
+                ======================
+                1. Cifrado Cesar
+                2. Descifrado Cesar
+                3. Cifrado Vigenere
+                4. Descifrado Vigenere
+                
+                Escoja una opción:
+                """;
+        System.out.println(menuText);
+        int opc = sc.nextInt();
+
+        switch (opc) {
+            case 1:
+                String palabra = sc.nextLine();
+                System.out.println("Ingrese la clave: ");
+                int clave= sc.nextInt();
+                cifradoCesar(palabra,clave);
+                break;
+            case 2:
+                descifradoCesar();
+                break;
+            case 3:
+                cifradoVigenere();
+                break;
+            case 4:
+                descifradoVigenere();
+                break;
+            default:
+                System.out.println("Opción no válida");
+        }
     }
 
-
-    public static void CifradoCesar(String palabra, int clave){
+    public static void cifradoCesar(String palabra, int clave){
         palabra = palabra.toLowerCase();
         char[] arrPalabra = palabra.toCharArray();
-        char[] decifrada = new char[0];
+        char[] descifrada = new char[0];
         for(int i = 0; i < palabra.length(); i++){
             for(int j = i + 1; j <= i; j++) {
                 if (arrPalabra[i] == alf[j]){
-                    decifrada[i] = alf[j + clave];
+                    descifrada[i] = alf[j + clave];
                 }
             }
-
-
         }
-        System.out.println( decifrada);
-
+        System.out.println(descifrada);
     }
 
-    public static void CifradoVigenere(String palabra, String clave ){
-        palabra = palabra.toLowerCase();
+    private static void descifradoCesar() {
+    }
 
+    public static void cifradoVigenere(){
+    }
 
+    private static void descifradoVigenere() {
     }
 }
